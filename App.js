b9af96db94a568c95538be93bb635500b7deaa38
Ol-create <oluola96@gmail.com>
2024-04-25 01:34:51 +0100

    add logo

--- App.js
+++ App.js
@@ -1,20 +1,24 @@
+import React from "react";
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button, Image, Alert } from 'react-native';
+import WelcomeScreen from './app/screens/WelcomeScreen';
+// import { useAccessibilityInfo } from "@react-native-community/hooks";
 
 export default function App() {
+  function handlePress() {
+    console.log("Text Clicked!")
+  }
+  // console.log(require("../app/assets/adaptive-icon.png"));
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+  <WelcomeScreen />
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     backgroundColor: 'blue',
+//     alignItems: 'center',
+//     justifyContent: 'center', 
+//   },
+// });
