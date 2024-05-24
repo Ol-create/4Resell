bbfd7cab32c2e07cce95036941aa9ee1a96f0cbf
Ol-create <oluola96@gmail.com>
2024-05-24 13:31:03 +0100

    add AppFormPicker component

---
+++ app/component/AppFormPicker.jsx
@@ -0,0 +1,22 @@
+import { StyleSheet, Text, View, FlatList } from 'react-native'
+import React from 'react'
+
+import AppPicker from "./AppPicker"
+import { useFormikContext } from 'formik'
+import { ErrorMessage } from './forms'
+
+
+export default function AppFormPicker({ items, name, placeholder }) {
+    const { errors, setFieldValue, values, touched } = useFormikContext();
+  return (
+      <>
+          <AppPicker items={items}
+              onSelectItem={(item) => setFieldValue(name, item)}
+              placeholder={placeholder}
+              selectItem={values[name]} />
+          <ErrorMessage error={errors[name]} visible={touched[name]} />
+      </>
+  )
+}
+
+const styles = StyleSheet.create({})
